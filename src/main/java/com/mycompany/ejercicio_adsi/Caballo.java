587092e68271bf819266122b992d5a5e772d18bd
bohorquezitos24 <nbohorquez24@misena.edu.co>
2021-03-03 21:31:36 -0500

    Se crea el constructor y los guetter and settter

--- src/main/java/com/mycompany/ejercicio_adsi/Caballo.java
+++ src/main/java/com/mycompany/ejercicio_adsi/Caballo.java
@@ -16,5 +16,49 @@ public class Caballo {
     private double tamaño;
     private double peso;
      //stive 
+
+    public Caballo(String nombre, int edad, double tamaño, double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tamaño = tamaño;
+        this.peso = peso;
+    }
+
+    public Caballo() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
   
+    
 }
+
